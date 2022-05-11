9404fa4ff52bd4a8e1dccb5bcf85c63b8d6b70c3
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-05-11 16:34:15 -0300

    Add Example 2

--- 1-DynamicAllocation.c
+++ 1-DynamicAllocation.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node
+{
+    int number;
+    struct node *prox;
+};
+typedef struct node Node;
+
 int main()
 {
     // Example 1
@@ -20,5 +27,34 @@ int main()
     }
     free(numbers);
 
+    // Example 2
+    Node *node = NULL;
+
+    for (int i = 0; i < size; i++)
+    {
+        Node *newNode = (Node *)malloc(sizeof(Node));
+        newNode->number = i;
+        newNode->prox = node;
+        node = newNode;
+    }
+
+    Node *i;
+    for (i = node; i != NULL; i = i->prox)
+    {
+        printf("node[%d]->number == %d\n", i, i->number);
+    }
+    free(node);
+
     return 0;
+
+    // In case there is no memory left, malloc will return NULL
+    /*
+        v = (float*) malloc(n*sizeof(float));
+        if (v==NULL)
+        {
+            printf("Memoria insuficiente.\n”);
+            return 1;
+        }
+        free(v);
+    */
 }
